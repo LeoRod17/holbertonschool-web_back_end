59190b464d9458d11fa8cc1902ab95df3028307c
LeoRod17 <matiassotelo171097@gmail.com>
2023-08-28 15:50:00 -0700

    task 7,8 fix

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ import math
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """a function that returns a text and a float in a tuple"""
     sq: float = v**2
-    return [k, sq]
+    return (k, sq)
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,7 +2,8 @@
 """a function that returns a float multiplied by itself"""
 from typing import Callable
 
-def make_multiplier(multiplier: float) -> Callable[[float],float]:
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """a function that returns a float multiplied by itself"""
     def mult(num: float) -> float:
         res = num * multiplier
