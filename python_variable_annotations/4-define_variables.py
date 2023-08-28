81c1e68da1868b7415f22785c83ba3ce9c3288fa
LeoRod17 <matiassotelo171097@gmail.com>
2023-08-28 14:37:43 -0700

    task 4 v3

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """giving value to variables"""
 
-a = int(1)
-pi = float(3.14)
-i_understand_annotations = bool(True)
-school = "Holberton"
+a: int =  1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
