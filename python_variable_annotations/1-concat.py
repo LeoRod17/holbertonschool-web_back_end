71120547ee0a9743d0970b98537c9d4e04840848
LeoRod17 <matiassotelo171097@gmail.com>
2023-08-28 13:52:11 -0700

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Making an add Strings function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """a function that takes a string str1 and a string str2 as
+    arguments and returns a concatenated string"""
+    ret = str1 + str2
+    return ret
