65821b0460f84045893ecfc2c3d7ae2a28228dee
LeoRod17 <matiassotelo171097@gmail.com>
2023-08-28 15:51:11 -0700

    task 7 pycode fix

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,7 @@
 from typing import Tuple, Union
 import math
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """a function that returns a text and a float in a tuple"""
     sq: float = v**2
