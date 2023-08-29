0b029b2ddc3220b02f5e2b3811ebcbef3840ed52
root <matiassotelo171097@gmail.com>
2023-08-29 07:05:16 -0700

    task 1 pycode fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,10 +8,9 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n:int, max_delay: int)->List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """making the function"""
     lista: List[float] = []
     for x in range(0, n):
         lista.append(await(wait_random(max_delay)))
     return sorted(lista)
-
