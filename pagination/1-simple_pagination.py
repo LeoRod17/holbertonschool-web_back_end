2f369519c0b910b81762f5ebae61e51c9fee4a4f
root <matiassotelo171097@gmail.com>
2023-09-04 10:56:51 -0700

    task 1 simple_pagination v3

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Copy index_range from the previous task and the following class into your code"""
+"""Copy index_range from the previous task and the
+following class into your code"""
 import csv
 import math
 from typing import List, Tuple
@@ -21,24 +22,23 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """You have to use this CSV file (same as the one
-            presented at the top of the project)
-            Use assert to verify that both arguments are integers greater than
-            0.
-            Use index_range to find the correct indexes to paginate the
-            dataset correctly and return the appropriate page of the dataset
-            (i.e. the correct list of rows).
-            If the input arguments are out of range for the dataset, an empty
-            list should be returned."""
-            index_range = __import__('0-simple_helper_function').index_range
-            assert isinstance(page, int)
-            assert page > 0
-            assert isinstance(page_size, int)
-            assert page_size > 0
-            result: Tuple = index_range(page, page_size)
-            self.dataset()
-            return self.__dataset[result[0]:result[1]]
+        """You have to use this CSV file (same as the one
+        presented at the top of the project)
+        Use assert to verify that both arguments are integers greater than
+        0.
+        Use index_range to find the correct indexes to paginate the
+        dataset correctly and return the appropriate page of the dataset
+        (i.e. the correct list of rows).
+        If the input arguments are out of range for the dataset, an empty
+        list should be returned."""
+        index_range = __import__('0-simple_helper_function').index_range
+        assert isinstance(page, int)
+        assert page > 0
+        assert isinstance(page_size, int)
+        assert page_size > 0
+        result: Tuple = index_range(page, page_size)
+        self.dataset()
+        return self.__dataset[result[0]:result[1]]
