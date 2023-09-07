b58d71254906e066c1a60d79fb7cd1796a9646b0
root <matiassotelo171097@gmail.com>
2023-09-07 07:51:36 -0700

    task 12 nosql v3

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
     print(collection.count_documents({}), "logs")
+    print("Methods:")
     print("\tmethod GET:",collection.count_documents({"method": "GET"}))
     print("\tmethod POST:",collection.count_documents({"method": "POST"}))
     print("\tmethod PUT:",collection.count_documents({"method": "PUT"}))
