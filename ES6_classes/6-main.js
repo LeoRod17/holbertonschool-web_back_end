191fbdfb189a359c3981063495bd6a39ef3b1a3a
LeoRod17 <Matiassotelo171097@gmail.com>
2023-09-15 09:56:04 -0700

    task 6 es6_class fix

--- ES6_classes/6-main.js
+++ ES6_classes/6-main.js
@@ -1,11 +1,6 @@
 import SkyHighBuilding from './6-sky_high.js';
 
-test("SkyHighBuilding is implemented correctly", () => {
-    const building = new SkyHighBuilding(200, 65);
-
-    expect(building.sqft).toBe(200);
-    expect(building.floors).toBe(65);
-
-    expect(typeof building.evacuationWarningMessage).toBe("function");
-    expect(building.evacuationWarningMessage()).toBe("Evacuate slowly the 65 floors");
-});
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
