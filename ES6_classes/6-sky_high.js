f16a383a855afb12469866673abf01afd7632946
LeoRod17 <Matiassotelo171097@gmail.com>
2023-09-15 06:56:11 -0700

    task 6 classes v3

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,11 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof (floors) === 'number') {
-      this._floors = floors;
-    } else {
-      throw TypeError('TypeError: floors must be a number');
-    }
+    this._floors = floors;
   }
 
   get sqft() {
