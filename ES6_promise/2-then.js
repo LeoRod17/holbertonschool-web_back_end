0f13cd78bf448c8a8332178d64ea8980361104f2
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-03 06:19:25 -0700

    task 2 catch me if you can v2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(promise)
-    .catch(Error)
-    .finally(console.log('Got a response from the API'))
+    .then(() => Promise.resolve({ status: 200, body: 'Success' }))
+    .catch(Error())
+    .finally(console.log('Got a response from the API'));
 }
