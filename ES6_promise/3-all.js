8e932b2f58ce33c489babb5331579755f2af3097
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-04 05:41:35 -0700

    task 3 promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,9 @@
 import { uploadPhoto, createUser } from "./utils";
+
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto, createUser]).then((values) => {
-    console.log(values);
-  });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(data[0].body, data[1].firstName, data[1].lastName)
+    })
+
 }
