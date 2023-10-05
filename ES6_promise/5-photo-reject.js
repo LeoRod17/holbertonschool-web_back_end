e39263081fa170a7a8d20c9e179e1c7fda8226eb
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-05 05:39:50 -0700

    task 5 promises v2

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,3 @@
 export default function uploadPhoto(filename) {
-  Promise.reject(Error).then()
-    .catch(() => { console.log(`${filename} cannot be processed`) });
+  return Promise.reject(Error(`${filename} cannot be processed`));
 }
