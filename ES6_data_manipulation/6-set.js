e7d90a56856d67043d86707e64fca829cb0fb9dc
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-10 09:37:10 -0700

    task 6 lint fix

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 export default function setFromArray(array) {
-    const set = new Set();
-    for (let x of array) {
-        set.add(x);
-    }
-    return set;
+  const set = new Set();
+  for (const x of array) {
+    set.add(x);
+  }
+  return set;
 }
