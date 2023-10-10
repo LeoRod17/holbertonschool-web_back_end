056a76cddc7fbf391e4c7c53419ee83b843dad99
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-10 10:27:59 -0700

    task 7 lint fix

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,9 +1,9 @@
 export default function hasValuesFromArray(set, array) {
-    for (const x of array) {
-        let flag = set.has(x);
-        if (flag == false) {
-            return false;
-        }
+  for (const x of array) {
+    const flag = set.has(x);
+    if (flag === false) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
