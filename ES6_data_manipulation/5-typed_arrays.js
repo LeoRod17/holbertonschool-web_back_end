1e9af7ddbe3e66887cc0d43032e0b2438b9f0982
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-10 09:16:34 -0700

    task 5 + Readme(modified)

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  const lista = new Int8Array(length);
+  if (position > length) {
+    throw error('Position outside range');
+  }
+  lista[position] = value;
+  const { buffer } = lista;
+  return new DataView(buffer, 0, length);
+}
