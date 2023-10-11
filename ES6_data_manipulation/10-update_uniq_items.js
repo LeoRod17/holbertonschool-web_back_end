89ccc42a3100e272c95ec2f94885dad2d1133016
LeoRod17 <matiassotelo171097@gmail.com>
2023-10-11 05:26:36 -0700

    task 10 lint fix

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,11 @@
 export default function updateUniqueItems(map) {
   if (map instanceof Map) {
-    for (let [key, value] of map) {
+    for (const [key, value] of map) {
       if (value === 1) {
         map.set(key, 100);
       }
     }
-  }
-  else {
+  } else {
     throw Error('Cannot process');
   }
 }
